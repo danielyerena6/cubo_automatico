bbf4acb6b9efe57da70a637d73cef105ecb1125a
Daniel <blasterdaxx2208@gmail.com>
2020-10-15 15:11:32 -0500

    Solo genera la primera rama del arbol de solucion

---
+++ cubo/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace cubo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Estado Inicial";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(26, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(132, 104);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Jugar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(26, 310);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(132, 310);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 7;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(26, 338);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 8;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(132, 338);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Estado Final";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(29, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(380, 76);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(143, 292);
+            this.listBox1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ cubo/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cubo
+{
+    public partial class Form1 : Form
+    {
+
+        string[,,] arbol = new string[2, 2, 100];
+        int ultimo_cubo = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox5.Text = "B";
+            textBox6.Text = "3";
+            textBox7.Text = "1";
+            textBox8.Text = "2";
+            label4.Text = "";
+            listBox1.Items.Clear();
+
+            for(int i=0;i<2;i++)
+            {
+                for(int j=0;j<2;j++)
+                {
+                    for(int k=0;k<100; k++)
+                    {
+                        arbol[i,j,k] = "0";
+                    }
+                }
+
+            }
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[,] cubo =new string [2,2];
+
+
+            cubo[0,0] = textBox1.Text;
+            cubo[0,1] = textBox2.Text;
+            cubo[1,0] = textBox3.Text;
+            cubo[1,1] = textBox4.Text;
+
+            int bien= fichas_correctas(cubo);
+            if (bien==1)
+            {
+                label4.Text = "";
+                movimientos(cubo);
+            }
+            else
+            {
+                label4.Text = "Digitos Incorrectos";
+            }
+            
+
+
+            
+
+        }
+
+        public int ultimo_cub()
+        {
+            
+            for (int k = 0; k < 100; k++)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        if(arbol[i,j,k].Equals("0"))
+                        {
+                            
+                            return k;
+                            
+                        }
+                    }
+                }
+
+            }
+
+            return 0;
+
+        }
+
+        public void movimientos(string[,] cubo)
+        {
+            ultimo_cubo = ultimo_cub();
+            string[,] cubo_auxiliar = new string[2, 2];
+            string auxiliar;
+            string elemento_lista="";
+            for(int i = 0; i < 2; i++)
+            {
+                for(int j=0;j<2;j++)
+                {
+                    cubo_auxiliar[i, j] = cubo[i, j];
+                }
+            }
+
+
+
+            for(int i = 0; i < 2; i++)
+            {
+                for(int j=0;j<2;j++)
+                {
+                    if (cubo_auxiliar[i, j].Equals("B"))
+                    {
+                        try
+                        {
+                            auxiliar = cubo_auxiliar[i + 1, j];
+                            cubo_auxiliar[i + 1, j] = cubo_auxiliar[i, j];
+                            cubo_auxiliar[i, j] = auxiliar;
+
+                            for (int fila = 0; fila < 2; fila++)
+                            {
+                                for (int columna = 0; columna < 2; columna++)
+                                {
+                                    arbol[fila, columna,ultimo_cubo] = cubo[fila, columna];
+                                }
+                            }
+
+                            ultimo_cubo += 1;
+
+                            elemento_lista = elemento_lista + cubo_auxiliar[0, 0] + cubo_auxiliar[0, 1] + cubo_auxiliar[1, 0] + cubo_auxiliar[1, 1] + ",";
+                            
+                            for (int fila = 0; fila < 2; fila++)
+                            {
+                                for (int columna = 0; columna < 2; columna++)
+                                {
+                                    cubo_auxiliar[fila, columna] = cubo[fila, columna];
+                                }
+                            }
+
+
+
+                        }
+                        catch
+                        {
+                            
+                        }
+
+                        try
+                        {
+                            auxiliar = cubo_auxiliar[i - 1, j];
+                            cubo_auxiliar[i - 1, j] = cubo_auxiliar[i, j];
+                            cubo_auxiliar[i, j] = auxiliar;
+
+                            for (int fila = 0; fila < 2; fila++)
+                            {
+                                for (int columna = 0; columna < 2; columna++)
+                                {
+                                    arbol[fila, columna, ultimo_cubo] = cubo[fila, columna];
+                                }
+                            }
+
+                            ultimo_cubo += 1;
+
+                            elemento_lista = elemento_lista + cubo_auxiliar[0, 0] + cubo_auxiliar[0, 1] + cubo_auxiliar[1, 0] + cubo_auxiliar[1, 1] + ",";
+
+                            for (int fila = 0; fila < 2; fila++)
+                            {
+                                for (int columna = 0; columna < 2; columna++)
+                                {
+                                    cubo_auxiliar[fila, columna] = cubo[fila, columna];
+                                }
+                            }
+
+
+
+                        }
+                        catch
+                        {
+
+                        }
+
+                        try
+                        {
+                            auxiliar = cubo_auxiliar[i , j+1];
+                            cubo_auxiliar[i, j+1] = cubo_auxiliar[i, j];
+                            cubo_auxiliar[i, j] = auxiliar;
+
+                            for (int fila = 0; fila < 2; fila++)
+                            {
+                                for (int columna = 0; columna < 2; columna++)
+                                {
+                                    arbol[fila, columna, ultimo_cubo] = cubo[fila, columna];
+                                }
+                            }
+
+                            ultimo_cubo += 1;
+
+                            elemento_lista = elemento_lista + cubo_auxiliar[0, 0] + cubo_auxiliar[0, 1] + cubo_auxiliar[1, 0] + cubo_auxiliar[1, 1] + ",";
+
+                            for (int fila = 0; fila < 2; fila++)
+                            {
+                                for (int columna = 0; columna < 2; columna++)
+                                {
+                                    cubo_auxiliar[fila, columna] = cubo[fila, columna];
+                                }
+                            }
+
+
+
+                        }
+                        catch
+                        {
+
+                        }
+
+                        try
+                        {
+                            auxiliar = cubo_auxiliar[i , j-1];
+                            cubo_auxiliar[i , j-1] = cubo_auxiliar[i, j];
+                            cubo_auxiliar[i, j] = auxiliar;
+
+                            for (int fila = 0; fila < 2; fila++)
+                            {
+                                for (int columna = 0; columna < 2; columna++)
+                                {
+                                    arbol[fila, columna, ultimo_cubo] = cubo[fila, columna];
+                                }
+                            }
+
+                            ultimo_cubo += 1;
+
+                            elemento_lista = elemento_lista + cubo_auxiliar[0, 0] + cubo_auxiliar[0, 1] + cubo_auxiliar[1, 0] + cubo_auxiliar[1, 1] + ",";
+
+                            for (int fila = 0; fila < 2; fila++)
+                            {
+                                for (int columna = 0; columna < 2; columna++)
+                                {
+                                    cubo_auxiliar[fila, columna] = cubo[fila, columna];
+                                }
+                            }
+
+
+
+                        }
+                        catch
+                        {
+
+                        }
+
+                        listBox1.Items.Add(elemento_lista);
+                    }
+                }
+            }
+            
+        }
+
+        public int fichas_correctas(string[,] cubo)
+        {
+            string[] fichas_correctas = new string[4];
+            fichas_correctas[0] = "1";
+            fichas_correctas[1] = "2";
+            fichas_correctas[2] = "3";
+            fichas_correctas[3] = "B";
+
+            int aux= 0;
+
+            foreach (string ficha in fichas_correctas)
+            {
+                foreach (string casilla in cubo)
+                {
+                    if (casilla.Equals(ficha))
+                    {
+                        aux =1;
+                    }
+                    
+                }
+                if(aux==0)
+                {
+                    
+                    return 0;
+                }
+                aux = 0;
+            }
+
+            if(cubo[0,0].Equals(cubo[0,1]) || cubo[0, 0].Equals(cubo[1, 0]) || cubo[0, 0].Equals(cubo[1, 1]))
+            {
+                return 0;
+            }
+
+            if(cubo[0,1].Equals(cubo[1, 1]) || cubo[0, 1].Equals(cubo[1, 0]))
+            {
+                return 0;
+            }
+
+            if(cubo[1, 0].Equals(cubo[1, 1]))
+            {
+                return 0;
+            }
+            
+
+            
+
+           
+            
+
+
+            return 1;
+
+            
+
+
+
+        }
+    }
+}
